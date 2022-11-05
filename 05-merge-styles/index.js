4c7d131452dd2725fce22ea26fb67e94e9687c75
Nazarenko Michael <mihaaaaa435@gmail.com>
2022-11-05 13:23:33 +0300

    feat: done the task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,54 @@
+"use strict"
+
+const fs = require('fs');
+const path = require('path');
+let { COPYFILE_EXCL } = fs.constants;
+
+fs.stat(
+    path.join(__dirname, 'project-dist', 'bundle.css'),
+    function (err) {
+        if(!err) {
+            fs.unlink(
+                path.join(__dirname, 'project-dist', 'bundle.css'),
+                err => {
+                    if(err) throw err;
+                }
+            )
+        } else if (err.code === 'ENOENT') {
+            fs.writeFile(
+                path.join(__dirname, 'project-dist', 'bundle.css'),
+                '',
+                err => {
+                    if(err) throw err;
+                }
+            )
+        }
+    }
+)
+
+fs.readdir(
+    path.join(__dirname, 'styles'),
+    (err, files) => {
+        files.forEach(elem => {
+        
+            if(elem.endsWith('.css')){
+
+                fs.readFile(
+                    path.join(__dirname, 'styles', elem),
+                    'utf-8',
+                    (err, data) => {
+                        if(err) throw err;
+
+                        fs.appendFile(
+                            path.join(__dirname, 'project-dist', 'bundle.css'),
+                            data,
+                            err => {
+                                if(err) throw err;
+                        });
+                });
+                
+                // console.log(elem);
+            }
+        })
+    }
+)
