48ceb08f3ec5cdae8533fb40228bddf0a35f4476
Nazarenko Michael <mihaaaaa435@gmail.com>
2022-11-05 10:33:52 +0300

    feat: done the task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+"use strict"
 
+const fs = require('fs');
+const path = require('path');
+
+
+fs.stat(
+    path.join(__dirname, 'files-copy'),
+    function (err) {
+        if(!err) {
+            return;
+        } else if (err.code === 'ENOENT') {
+            fs.mkdir(
+                path.join(__dirname, 'files-copy'),
+                err => {
+                    if(err) throw err;
+                }
+            )
+        }
+    }
+)
+
+
+fs.readdir(
+    path.join(__dirname, 'files'),
+    (err, files) => {
+        files.forEach(elem => {
+            fs.copyFile(
+                path.join(__dirname, 'files', elem),
+                path.join(__dirname, 'files-copy',elem),
+                err => {
+                    if(err) {
+                        throw err;
+                    }
+                }
+            )
+        })
+    }
+)
